5daf18aadd7099b1dedb978d92faea0996b65da4
1kurs1 <lmtk161044@gmail.com>
2024-06-24 23:46:58 +0300

    entry point

--- .env.cmake
+++
@@ -1,5 +0,0 @@
-# для народной убунты
-
-set(TINYOBJ_PATH /mnt/9A4E79B34E798931/Projects/OtherProjects/Window/libs/tinyobjectloader)
-set(STB_PATH /mnt/9A4E79B34E798931/Projects/OtherProjects/Window/libs/stbimage)
-set(IMGUI_INCLUDE_PATH /mnt/9A4E79B34E798931/Projects/OtherProjects/Window/libs/imgui)
--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# ConfuseEngine
-This is Nicromis™ own 3D Engine
-
-### Build on Linux and MacOS:
-    ./unixBuild.sh
-
-### Build on Windows:
-- delete `.env.cmake` file
-- rename `envWindows.cmake` to `.env.cmake`
-- run `mingwBuild.bat`
+Confuse alpha version branch
